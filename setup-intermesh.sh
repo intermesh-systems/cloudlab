1ce295ca6ff4f496af47c132b863fdd9e94af36b
Lloyd Brown <lloydbrown@berkeley.edu>
2022-12-05 09:39:18 -0800

    Add docker pulls

--- setup-intermesh.sh
+++ setup-intermesh.sh
@@ -38,6 +38,30 @@ chmod 400 $INTERMESH_KEY
 chmod 400 $DEATHSTARBENCH_KEY
 git clone git@github.com:intermesh-systems/im-deathstarbench.git --config core.sshCommand="ssh -i $DEATHSTARBENCH_KEY"
 pushd im-deathstarbench/hotelReservation/
+
+	# Put the docker images for jaeger, consul, memcached, mongo into the local registry.
+	# First pull from docker hub.
+	docker pull consul:latest
+	# Then tag and push to local registry.
+	docker tag consul:latest localhost:5001/consul:latest
+	# Push to local registry.
+	docker push localhost:5001/consul:latest
+
+	# Do the same for jaeger.
+	docker pull jaegertracing/all-in-one:latest
+	docker tag jaegertracing/all-in-one:latest localhost:5001/jaegertracing/all-in-one:latest
+	docker push localhost:5001/jaegertracing/all-in-one:latest
+
+	# Do the same for memcached.
+	docker pull memcached:latest
+	docker tag memcached:latest localhost:5001/memcached:latest
+	docker push localhost:5001/memcached:latest
+
+	# Do the same for mongo.
+	docker pull mongo:4.4.6
+	docker tag mongo:4.4.6 localhost:5001/mongo:4.4.6
+	docker push localhost:5001/mongo:4.4.6
+
 	echo "running docker build"
 	sudo bash ./docker_scripts/build-docker-images.sh
 	echo "done with docker build"
